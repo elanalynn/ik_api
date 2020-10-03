805c6e448b355d7a3b35294a2f32136c855f5dff
Elana Kopelevich <ekopelevich@gmail.com>
2020-10-02 20:05:17 -0600

    adjust cors restrictions

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,8 +9,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'irinakopelevich.com'
 
-    resource '/orders',
+    resource '*',
       headers: :any,
-      methods: [:post]
+      methods: [:post, :head, :options]
   end
 end
